76895a56916bac6a868172b575cffe5e1b6f1ad5
luis-f-g169 <156151159+luis-f-g169@users.noreply.github.com>
2024-01-10 08:54:46 -0800

    Add files via upload

--- planets.py
+++ planets.py
@@ -2,9 +2,19 @@
 # If string does not match a planet, raise ValueError
 def weight_on_planets(pounds: float, planet: str) -> float:
    # write your code here
-   return 0.0
+   new_weight = 0
+   if planet == "Mars":
+      new_weight = pounds * .38
+   elif planet == "Jupiter":
+      new_weight = pounds * 2.34
+   elif planet == "Venus":
+      new_weight = pounds * .91
+   else:
+      raise ValueError
+   return new_weight
 
 if __name__ == '__main__':    # pragma: no cover
    pounds = float(input("What do you weigh on earth? "))
    print("\nOn Mars you would weigh", weight_on_planets(pounds, 'Mars'), "pounds.\n" +
+         "\nOn Venus you would weigh", weight_on_planets(pounds, 'Venus'), "pounds.\n" +
           "On Jupiter you would weigh", weight_on_planets(pounds, 'Jupiter'), "pounds.")
